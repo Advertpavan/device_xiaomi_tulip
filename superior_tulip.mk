c82b63a782f0c088e07f73efa9383b756544731a
Advertpavan <pvnptl98@gmail.com>
2019-04-16 12:57:33 +0000

    tulip:Superior Bringup

--- superior_tulip.mk
+++ superior_tulip.mk
@@ -22,10 +22,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit from tulip device
 $(call inherit-product, device/xiaomi/tulip/device.mk)
 
-# Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+# Inherit some common Superior stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/superior/config/common.mk)
 
-PRODUCT_NAME := mk_tulip
+PRODUCT_NAME := superior_tulip
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := tulip
 PRODUCT_MANUFACTURER := Xiaomi
